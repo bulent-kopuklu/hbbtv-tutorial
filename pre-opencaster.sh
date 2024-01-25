52b75a04c3742a5e7e08fdc835389e7c672a70b8
bulent-kopuklu <bulent.kopuklu@gmx.com.tr>
2024-01-25 20:04:23 +0300

    docker

--- pre-opencaster.sh
+++ pre-opencaster.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker build -t encom/opencaster-builder opencaster
-docker run --name encom-opencaster-builder -v opencaster:/build-out encom/opencaster-builder
+
+docker run --rm -v $PWD/opencaster:/build-out encom/opencaster-builder
+
 ln -s opencaster/dvbobjects dvbobjects
-
-
 mkdir output
